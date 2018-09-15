4faa0db5d80ef09fc72aa10901100cb0ef818a2b
fengwei232 <240787860@qq.com>
2018-09-15 12:57:37 +0800

    第三次commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+import Home from './view/home/index'
+import Login from './view/login/index'
+import Worke from './view/work/index'
+import Message from './view/message/index'
+import Course from './view/course/index'
+import Lecturer from './view/lecturer/index'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/login" exact component={Login}/>
+        <Route path="/work" exact component={Worke}/>
+        <Route path="/message" exact component={Message}/>
+        <Route path="/course" exact component={Course}/>
+        <Route path="/lecturer" exact component={Lecturer}/>
+      </Switch>
     );
   }
 }
